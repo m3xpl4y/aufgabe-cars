e95c09b5f39f1d3134de604111fd0392a858efdb
m3xpl4y <m3xpl4y@gmail.com>
2021-04-13 14:27:34 +0200

    Engine class updated

--- src/at/maximilian/cars/Engine.java
+++ src/at/maximilian/cars/Engine.java
@@ -1,4 +1,15 @@
 package at.maximilian.cars;
 
 public class Engine {
+    private int horsePower;
+    public enum TYPE {DIESEL, GAS};
+
+    //Constructor
+    public Engine(int horsePower) {
+        this.horsePower = horsePower;
+    }
+    //Getter
+    public int getHorsePower() {
+        return horsePower;
+    }
 }
