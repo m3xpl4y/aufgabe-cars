7d00db6f81f853ee801b5926475957024799add6
m3xpl4y <m3xpl4y@gmail.com>
2021-04-13 12:28:09 +0200

    added Car properties

--- src/at/maximilian/cars/Car.java
+++ src/at/maximilian/cars/Car.java
@@ -1,4 +1,11 @@
 package at.maximilian.cars;
 
 public class Car extends Vehicle{
+    private Engine engine;
+    private Producer producer;
+    private Trunk trunk;
+    private Tire tires;
+    private Mirror mirrors;
+    private String color;
+
 }
