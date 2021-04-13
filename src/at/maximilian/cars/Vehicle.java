ee8480ad7d0b752a0206636e114aee4e63ef4649
m3xpl4y <m3xpl4y@gmail.com>
2021-04-13 12:06:32 +0200

    added properties

--- src/at/maximilian/cars/Vehicle.java
+++ src/at/maximilian/cars/Vehicle.java
@@ -1,4 +1,10 @@
 package at.maximilian.cars;
 
 public class Vehicle {
+    private double restFuel;
+    private double drivenKM;
+    private int constructionYear;
+    private int seats;
+    private enum DRIVER_LICENCE{A,B,C};
+
 }
