26389d1c667b0513150e51b950c99a4ee751b7fe
m3xpl4y <m3xpl4y@gmail.com>
2021-04-13 15:06:09 +0200

    Producer class updated

--- src/at/maximilian/cars/Producer.java
+++ src/at/maximilian/cars/Producer.java
@@ -1,4 +1,27 @@
 package at.maximilian.cars;
 
 public class Producer {
+    private String companyName;
+    private String origin;
+    private int discount;
+
+    //Constructor
+    public Producer(String companyName, String origin, int discount) {
+        this.companyName = companyName;
+        this.origin = origin;
+        this.discount = discount;
+    }
+
+    //Getter
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
 }
