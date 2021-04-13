1037c45619368551e14a725c0c99dc9fd53ad1ae
m3xpl4y <m3xpl4y@gmail.com>
2021-04-13 14:30:09 +0200

    FuelTank class updated

--- src/at/maximilian/cars/FuelTank.java
+++ src/at/maximilian/cars/FuelTank.java
@@ -1,4 +1,21 @@
 package at.maximilian.cars;
 
 public class FuelTank{
+    int fuelCapacity;
+    double fuelConsummation;
+
+    //Constructor
+    public FuelTank(int fuelCapacity, double fuelConsummation) {
+        this.fuelCapacity = fuelCapacity;
+        this.fuelConsummation = fuelConsummation;
+    }
+
+    //Getter
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public double getFuelConsummation() {
+        return fuelConsummation;
+    }
 }
